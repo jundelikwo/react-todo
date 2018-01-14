68d0b433631fd424e8649e3e36a8a3c6557b1fbf
Jonathan Utsu Undelikwo <jundelikwo@gmail.com>
2018-01-14 16:36:43 +0100

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,14 +1,50 @@
+// import firebase from 'firebase'
+
+// if(process.env = 'test'){
+//     var config = require('config/test')
+// }else{
+//     var config = require('config/development')
+// }
+
+// console.log('Config',config)
+
+// try {
+//     // Initialize Firebase
+//     var config = {
+//         apiKey: config.API_KEY,
+//         authDomain: config.AUTH_DOMAIN,
+//         databaseURL: config.DATABASE_URL,
+//         projectId: config.PROJECTID,
+//         storageBucket: config.STORAGE_BUCKET,
+//         messagingSenderId: config.MESSAGINGSENDERID
+//     };
+//     firebase.initializeApp(config);
+
+// } catch (e) {
+
+// }
+
+// export var firebaseRef = firebase.database().ref()
+// export default firebase
+
+
 import firebase from 'firebase'
+
+if(process.env.NODE_ENV === 'test'){
+    var config = require('config/test')
+}else{
+    var config = require('config/development')
+}
 
 try {
     // Initialize Firebase
     var config = {
-        apiKey: "AIzaSyBRMrJ6epYZBMXlJqYlYUPQJggDzahRYnM",
-        authDomain: "react-todo-app-fbcef.firebaseapp.com",
-        databaseURL: "https://react-todo-app-fbcef.firebaseio.com",
-        projectId: "react-todo-app-fbcef",
-        storageBucket: "react-todo-app-fbcef.appspot.com",
-        messagingSenderId: "187551939735"
+        apiKey: config.API_KEY,
+        authDomain: config.AUTH_DOMAIN,
+        databaseURL: config.DATABASE_URL,
+        projectId: config.PROJECTID,
+        storageBucket: config.STORAGE_BUCKET,
+        messagingSenderId: config.MESSAGINGSENDERID
     };
     firebase.initializeApp(config);
 
